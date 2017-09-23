a406c7e672eeb1031b45e74c4184d3854fdffb91
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2017-09-23 10:00:09 +0700

    list of list

--- script/convert.py
+++ script/convert.py
@@ -6,17 +6,22 @@ with open(fname) as f:
     content = f.readlines()
 # content = [x.strip() for x in content] 
 
-list_of_tuple = []
+list_complete = []
+list_of_tuple = None
 
 for line in content:
 	if line[0] != '\n' and line[0] != '#':
+		if line[0:2] == '1\t':
+			list_complete.append(list_of_tuple)
+			list_of_tuple = []
 		tokens = line.split('\t')
 		list_of_tuple.append((tokens[1],tokens[3]))
 
-print(list_of_tuple)
+list_complete = list_complete[1:]
+print(list_complete)
 
 with open('../UD_Indonesian_Conll2017/list_of_tuple.pickle', 'wb') as handle:
-    pickle.dump(list_of_tuple, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(list_complete, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 # How to load file
 with open('../UD_Indonesian_Conll2017/list_of_tuple.pickle', 'rb') as handle:
@@ -24,4 +29,4 @@ with open('../UD_Indonesian_Conll2017/list_of_tuple.pickle', 'rb') as handle:
 
 
 # Check result
-print(list_of_tuple == loaded)
+print(list_complete == loaded)
